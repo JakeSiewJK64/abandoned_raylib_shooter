620aba2f1aafc5da981fa8814899febbfbbe897a
JakeSiewJK64 <joekanesiew@outlook.com>
2024-10-26 15:56:40 +0800

    update

--- include/player.h
+++ include/player.h
@@ -9,4 +9,16 @@ typedef struct GameObject {
 
 void UpdatePlayerPosition(GameObject *player);
 
+/**
+ * @brief Fires a bullet from the specified position.
+ *
+ * Adds a new bullet to the bullets array and increments the bullet count.
+ *
+ * @param bullets Pointer to an array of Vector2 holding bullet positions.
+ * @param bullet_count Pointer to the current number of bullets; updated after
+ * firing.
+ * @param bullet_spawn_position Position where the bullet is spawned.
+ *
+ * @note Ensure the bullets array has enough space before calling this function.
+ */
 void Fire(Vector2 *bullets, int *bullet_count, Vector2 bullet_spawn_position);
