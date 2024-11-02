ea916fc01eb21ab8380beb9f26d3a7214a56dd0f
JakeSiewJK64 <joekanesiew@outlook.com>
2024-11-02 16:45:50 +0800

    update

--- src/utils.c
+++ src/utils.c
@@ -23,7 +23,8 @@ int UpdateBulletPosition(Bullet bullets[], int *bullet_count) {
   return 0;
 }
 
-int DrawBullets(Bullet bullets[], int *bullet_count, Color color, float radius) {
+int DrawBullets(Bullet bullets[], int *bullet_count, Color color,
+                float radius) {
   for (int i = 0; i < *bullet_count; i++) {
     DrawCircleV(bullets[i].position, radius, color);
   }
